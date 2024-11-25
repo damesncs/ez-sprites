34fdf5d99f86a2d027af799ea467bd1fd8085f09
david <dames@noteablenow.com>
2024-11-25 12:13:09 -0500

    working towards breakout game

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const CANVAS_HEIGHT = 300;
 
 const BALL_RADIUS = 10;
 
-const PADDLE_HEIGHT = 50;
-const PADDLE_WIDTH = 10;
+const PADDLE_HEIGHT = 10;
+const PADDLE_WIDTH = 50;
 const PADDLE_SPEED = 4;
 
 const PADDLE_COLOR = "blue";
@@ -14,16 +14,15 @@ const EVENT_KEY_RELEASED = "keyup";
 
 const SPRITES = [];
 
-const BALLS = [];
+const BRICKS = [];
 
 let canvas;
 let ctx;
 window.onload = start;
 
-let paddleL, paddleR;
+let ball, paddle;
 
-let playerOneScore = 0;
-let playerTwoScore = 0;
+let playerScore = 0;
 
 function start() {
     canvas = document.getElementById("canvas");
@@ -31,8 +30,7 @@ function start() {
     canvas.height = CANVAS_HEIGHT;
     ctx = canvas.getContext("2d");
 
-    paddleL = createRectSprite(0, 0, 0, 0, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_COLOR);
-    paddleR = createRectSprite(CANVAS_WIDTH - PADDLE_WIDTH, 0, 0, 0, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_COLOR);
+    paddle = createRectSprite(0, CANVAS_HEIGHT - PADDLE_HEIGHT, 0, 0, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_COLOR);
 
     createNewBall();
 
@@ -47,84 +45,94 @@ function drawEachFrame(){
     drawBorder();
     handleCollisions();
     moveAndDrawSprites();
-    drawScores();
-}
-
-function drawScores(){
-    drawText(CANVAS_WIDTH / 3, CANVAS_HEIGHT / 2 - 18, playerOneScore, 36, "black");
-    drawText(CANVAS_WIDTH - (CANVAS_WIDTH / 3), CANVAS_HEIGHT / 2 - 18, playerTwoScore, 36, "black");
-}
-
-function createNewBall(){
-    const r1 = Math.random() * 3;
-    const r2 = Math.random() * 3;
-    const color = getRandomColorHexString();
-    const ball = createCircleSprite(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, r1, r2, BALL_RADIUS, color);
-    BALLS.push(ball);
-}
-
-function handleCollisions(){
-    BALLS.forEach(b => handleBallPaddleCollisions(b, paddleL, paddleR));
-}
-
-function handleBallPaddleCollisions(b, pL, pR){
-    if (b.leftEdge < 0){
-        // past left edge
-        playerTwoScore++;
-        b.dx = -b.dx;
-        createNewBall();
-        resetBall(b);
-    }
-
-    if (b.rightEdge > CANVAS_WIDTH){
-        // past right edge
-        playerOneScore++;
-        b.dx = -b.dx;
-        createNewBall();
-        resetBall(b);
-    }
-
-    // check if ball is colliding with top or bottom wall
-    if (b.topEdge < 0 || b.bottomEdge > CANVAS_HEIGHT){
-        // reverse y direction
-        b.dy = -b.dy;
-    }
-
-    // bounce on Right paddle
-    if (b.rightEdge > pR.leftEdge &&
-        b.y > pR.topEdge &&
-        b.y < pR.bottomEdge){
-        b.dx = -b.dx;
-    }
-
-    // bounce on Left paddle
-    if (b.leftEdge < pL.rightEdge &&
-        b.y > pL.topEdge &&
-        b.y < pL.bottomEdge){
-        b.dx = -b.dx;
-    }
+    // drawScores();
 }
 
 function onKeyEvent(e){
     // console.log(e);
-    if(e.code === "ArrowDown"){
-        onKeyEventArrowDown(e.type);
-    } 
-    else if (e.code === "ArrowUp"){
-        onKeyEventArrowUp(e.type);
-    } 
-    else if (e.code === "KeyS"){
-        onKeyEventS(e.type);
+    if(e.code === "ArrowRight"){
+        onKeyEventArrowRight(e.type);
     }
-    else if (e.code === "KeyW"){
-        onKeyEventW(e.type);
-    } 
+    else if (e.code === "ArrowLeft"){
+        onKeyEventArrowLeft(e.type);
+    }
     // otherwise, do nothing
+}
+
+// function drawScores(){
+//     drawText(CANVAS_WIDTH / 3, CANVAS_HEIGHT / 2 - 18, playerOneScore, 36, "black");
+//     drawText(CANVAS_WIDTH - (CANVAS_WIDTH / 3), CANVAS_HEIGHT / 2 - 18, playerTwoScore, 36, "black");
+// }
+
+function createNewBall(){
+
+    const color = getRandomColorHexString();
+    ball = createCircleSprite(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 3, 3, BALL_RADIUS, color);
+}
+
+function handleCollisions(){
+    handleBallWallCollisions(ball);
+    handleBallPaddleCollisions(ball, paddle);
+    BRICKS.forEach(b => handleBallBrickCollisions(ball, BALL_RADIUS, b));
+}
+
+function handleBallWallCollisions(b){
+    if (b.leftEdge < 0){
+        // bounce
+        b.dx = -b.dx;
+    }
+
+    if (b.rightEdge > CANVAS_WIDTH){
+        // bounce
+        b.dx = -b.dx;
+    }
+
+    // check if ball is colliding with top wall
+    if (b.topEdge < 0){
+        // reverse y direction
+        b.dy = -b.dy;
+    }
+
+    if(b.bottomEdge > CANVAS_HEIGHT){
+        // ball went past bottom, player loses
+        // TODO game over
+        resetBall(b);
+    }
+}
+
+function handleBallPaddleCollisions(b, p){
+    if(b.bottomEdge > p.topEdge &&
+        b.x > p.leftEdge &&
+        b.x < p.rightEdge){
+        b.dy = -b.dy;
+    }
+}
+
+function handleBallBrickCollisions(b, radius, brick){
+
 }
 
 function resetBall(b){
     b.x = CANVAS_WIDTH / 2;
     b.y = CANVAS_HEIGHT / 2;
+}
+
+function onKeyEventArrowLeft(eventType){
+    if(eventType === EVENT_KEY_PRESSED){
+        paddle.dx = -PADDLE_SPEED;
+    }
+    if(eventType === EVENT_KEY_RELEASED){
+        paddle.dx = 0;
+    }
+}
+
+function onKeyEventArrowRight(eventType){
+    if(eventType === EVENT_KEY_PRESSED){
+        paddle.dx = PADDLE_SPEED;
+    }
+    if(eventType === EVENT_KEY_RELEASED){
+        paddle.dx = 0;
+    }
 }
 
 function createRectSprite(x, y, dx, dy, width, height, color){
@@ -165,50 +173,6 @@ function moveAndDrawSprites(){
         s.topEdge = edges.topEdge;
         s.bottomEdge = edges.bottomEdge
     });
-}
-
-function onKeyEventArrowDown(eventType){
-    if(eventType === EVENT_KEY_PRESSED){
-        // start moving right paddle down
-        paddleR.dy = PADDLE_SPEED;
-    }
-    if(eventType === EVENT_KEY_RELEASED){
-        // stop moving right paddle
-        paddleR.dy = 0;
-    }
-}
-
-function onKeyEventArrowUp(eventType){
-    if(eventType === EVENT_KEY_PRESSED){
-        // start moving right paddle up
-        paddleR.dy = -PADDLE_SPEED;
-    }
-    if(eventType === EVENT_KEY_RELEASED){
-        // stop moving right paddle
-        paddleR.dy = 0;
-    }
-}
-
-function onKeyEventS(eventType){
-    if(eventType === EVENT_KEY_PRESSED){
-        // start moving left paddle down
-        paddleL.dy = PADDLE_SPEED;
-    }
-    if(eventType === EVENT_KEY_RELEASED){
-        // stop moving left paddle
-        paddleL.dy = 0;
-    }
-}
-
-function onKeyEventW(eventType){
-    if(eventType === EVENT_KEY_PRESSED){
-        // start moving left paddle up
-        paddleL.dy = -PADDLE_SPEED;
-    }
-    if(eventType === EVENT_KEY_RELEASED){
-        // stop moving left paddle
-        paddleL.dy = 0;
-    }
 }
 
 function drawBorder(){
