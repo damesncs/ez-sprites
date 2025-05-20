c3852a47ecc9617420532c6cf1a752cd6ef8d057
damesncs <167118878+damesncs@users.noreply.github.com>
2025-05-20 10:19:54 -0400

    Update stickman.js

--- svg-four-state-sprite/js/stickman.js
+++ svg-four-state-sprite/js/stickman.js
@@ -1,28 +1,27 @@
-// svg sprites starter
+// sprite movement demo #3
 
 import {
     clearCanvas,
     createRectSprite,
     createSpriteFromSvg,
     drawBorder,
-    getRandomColorHexString,
     moveAndDrawSprites,
     pathArrayFromSvg,
     rectOverlapsRect,
     setupCanvas
 } from "/ez-sprites/js/ez-sprites.js";
 
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 500;
-
+const CANVAS_WIDTH = 900;
+const CANVAS_HEIGHT = 700;
 
 const EVENT_KEY_PRESSED = "keydown";
 const EVENT_KEY_RELEASED = "keyup";
 
+// character movement physics constants
 const CHARACTER_WALK_SPEED = 3;
 const CHARACTER_JUMP_SPEED = 5;
 const CHARACTER_GRAVITY = 0.15;
-
+const CHARACTER_BOUNCE_SPEED_LOSS = 3;
 
 window.onload = start;
 
@@ -44,13 +43,11 @@ CHARACTER_STANDING;
 async function start() {
     setupCanvas(document.getElementById("canvas"), CANVAS_HEIGHT, CANVAS_WIDTH);
 
-    // optionally load a static background from an svg file:
-    // setBackgroundFromSvg("svg/background.svg", CANVAS_WIDTH, CANVAS_HEIGHT);
-
     // create initial sprites here
     character = await createSpriteFromSvg(200, 200, 0, 0, 0.5, "svg/stickman_standing.svg");
 
     character.y = FLOOR_Y - character.height;
+    character.jumping = false;
 
     // load paths for alternate states
     CHARACTER_STANDING = await pathArrayFromSvg("svg/stickman_standing.svg");
@@ -59,24 +56,13 @@ async function start() {
     CHARACTER_JUMPING_LEFT = await pathArrayFromSvg("svg/stickman_jumping_left.svg");
     CHARACTER_JUMPING_RIGHT = await pathArrayFromSvg("svg/stickman_jumping_right.svg");
 
-    createRandomObstacles(10);
+    createRandomObstacles(5);
 
     setInterval(drawEachFrame, 15);
 
     addEventListener("keydown", onKeyEvent);
     addEventListener("keyup", onKeyEvent);
-    // add additional event handlers here (e.g., mouse events)
-}
-
-function setBackgroundFromSvg(path, width, height){
-    fetch(path).then(r => {
-        r.text().then(t =>{
-            const ctr = document.getElementById("background-container");
-            ctr.innerHTML = t;
-            ctr.style.width = width;
-            ctr.style.height = height;
-        });
-    });
+    addEventListener("mousedown", onMouseDown);
 }
 
 function drawEachFrame(){
@@ -87,33 +73,38 @@ function drawEachFrame(){
 }
 
 function checkSpriteCollisions(){
-    // here's a spot to check if sprites are colliding and handle those situations
-
     obstacles.forEach(obstacle => {
         let colliding = false;
-        if(spriteOverlapsRectRightEdge(character, obstacle)){
-            character.dx = 0;
-            character.x = obstacle.rightEdge;
-            colliding = true;
-        }
-        else if(spriteOverlapsRectLeftEdge(character, obstacle)){
-            character.dx = 0;
-            character.x = obstacle.leftEdge - character.width;
-            colliding = true;
-        }
-        else if(spriteOverlapsRectTopEdge(character, obstacle)){
-            character.dy = 0;
+        
+        if(spriteOverlapsRectTopEdge(character, obstacle)){
+            if(character.dy > 0){
+                character.dy = -(getSpeedFromCollision(character.dy));
+            }
             character.y = obstacle.topEdge - character.height;
             colliding = true;
+            // standing on an obstacle, reset jump
+            character.jumping = false;  
         }
+
         else if(spriteOverlapsRectBottomEdge(character, obstacle)){
-            character.dy = 0;
+            character.dy = getSpeedFromCollision(character.dy);
             character.y = obstacle.bottomEdge + 1;
             colliding = true;
         } 
         
+        else if(spriteOverlapsRectRightEdge(character, obstacle)){
+            character.dx = getSpeedFromCollision(character.dx);
+            character.x = obstacle.rightEdge;
+            colliding = true;
+        }
+
+        else if(spriteOverlapsRectLeftEdge(character, obstacle)){
+            character.dx = -(getSpeedFromCollision(character.dx));
+            character.x = obstacle.leftEdge - character.width;
+            colliding = true;
+        }
+        
         if(colliding){
-            // character.paths = character_MAD;
             obstacle.color = "red";
         } else {
             obstacle.color = "gray";
@@ -121,36 +112,53 @@ function checkSpriteCollisions(){
     });
 
     // apply gravity to character
-    if(character.bottomEdge <= CANVAS_HEIGHT){
+    if(character.bottomEdge <= FLOOR_Y){
         character.dy += CHARACTER_GRAVITY;
     } else {
-        // come back to rest on bottom edge since ball has gone beyond it
-        character.y = CANVAS_HEIGHT - character.height;
+        // come back to rest on bottom edge since character has gone beyond it
+        character.y = FLOOR_Y - character.height;
+        // reset jump
+        character.jumping = false;
     }
-
 }
 
 function spriteOverlapsRectRightEdge(sprite, rectToCheck){
-    return sprite.rightEdge > rectToCheck.rightEdge && rectOverlapsRect(sprite, rectToCheck);
+    return sprite.rightEdge > rectToCheck.rightEdge &&
+            sprite.leftEdge > rectToCheck.leftEdge &&
+             rectOverlapsRect(sprite, rectToCheck);
 }
 
 function spriteOverlapsRectLeftEdge(sprite, rectToCheck){
-    return sprite.leftEdge < rectToCheck.leftEdge && rectOverlapsRect(sprite, rectToCheck);
+    return sprite.leftEdge < rectToCheck.leftEdge &&
+            sprite.rightEdge < rectToCheck.rightEdge &&
+             rectOverlapsRect(sprite, rectToCheck);
 }
 
 function spriteOverlapsRectTopEdge(sprite, rectToCheck){
-    return sprite.topEdge < rectToCheck.topEdge && rectOverlapsRect(sprite, rectToCheck);
+    return sprite.topEdge < rectToCheck.topEdge &&
+            sprite.bottomEdge < rectToCheck.bottomEdge &&
+             rectOverlapsRect(sprite, rectToCheck);
 }
 
 function spriteOverlapsRectBottomEdge(sprite, rectToCheck){
-    return sprite.bottomEdge > rectToCheck.bottomEdge && rectOverlapsRect(sprite, rectToCheck);
+    return sprite.bottomEdge > rectToCheck.bottomEdge &&
+            sprite.topEdge > rectToCheck.topEdge &&
+             rectOverlapsRect(sprite, rectToCheck);
+}
+
+function getSpeedFromCollision(d){
+    const absSpeed = Math.abs(d);
+    if(absSpeed >= CHARACTER_BOUNCE_SPEED_LOSS){
+        return absSpeed - CHARACTER_BOUNCE_SPEED_LOSS;
+    }
+    return 0;
 }
 
 
 function createRandomObstacles(count){
     for(let i = 0; i < count; i++){
-        const width = getRandom(5, 50);
-        const height = getRandom(5, 50);
+        const width = getRandom(10, 70);
+        const height = getRandom(10, 20);
         const x = getRandom(0, CANVAS_WIDTH - width);
         const y = getRandom(0, CANVAS_HEIGHT - height);
         obstacles.push(createRectSprite(x, y, 0, 0, width, height, "gray"));
@@ -167,9 +175,6 @@ function onKeyEvent(e){
     else if (e.code === "ArrowUp"){
         onKeyEventArrowUp(e.type);
     }
-    // else if (e.code === "ArrowDown"){
-    //     onKeyEventArrowDown(e.type);
-    // } 
 }
 
 function onKeyEventArrowLeft(eventType){
@@ -196,35 +201,30 @@ function onKeyEventArrowRight(eventType){
 
 function onKeyEventArrowUp(eventType){
     if(eventType === EVENT_KEY_PRESSED){
-        character.dy = -CHARACTER_JUMP_SPEED;
-        if(character.dx > 0){
-            // moving right
-            character.paths = CHARACTER_JUMPING_RIGHT;
-        } else {
-            character.paths = CHARACTER_JUMPING_LEFT;
+        // jump
+        if(character.jumping == false){
+            character.jumping = true;
+            character.dy = -CHARACTER_JUMP_SPEED;
+            if(character.dx > 0){
+                // moving right, show jumping to the right
+                character.paths = CHARACTER_JUMPING_RIGHT;
+            } else {
+                // moving left, show jumping to the left
+                character.paths = CHARACTER_JUMPING_LEFT;
+            }
         }
-        
     }
     if(eventType === EVENT_KEY_RELEASED){
-        character.dy = 0;
         character.paths = CHARACTER_STANDING;
     }
+}
+
+function onMouseDown(e){
+    const width = getRandom(10, 70);
+    const height = getRandom(10, 20);
+    obstacles.push(createRectSprite(e.offsetX, e.offsetY, 0, 0, width, height, "gray"));
 }
 
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
-
-// function onKeyEventArrowDown(eventType){
-//     if(eventType === EVENT_KEY_PRESSED){
-//         // character.dy = CHARACTER_WALK_SPEED;
-//         character.paths = CH;
-//     }
-//     if(eventType === EVENT_KEY_RELEASED){
-//         character.dy = 0;
-//         character.paths = CHARACTER_STANDING;
-//     }
-// }
-
-
-
